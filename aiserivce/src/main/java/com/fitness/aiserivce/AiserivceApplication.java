04c4ff819d45da9404f72018aab06cf4d0cafa27
Amanbairagi17 <amanbairagi741@gmail.com>
2026-01-05 22:55:49 +0530

    Added AI Service

---
+++ aiserivce/src/main/java/com/fitness/aiserivce/AiserivceApplication.java
@@ -0,0 +1,13 @@
+package com.fitness.aiserivce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiserivceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiserivceApplication.class, args);
+	}
+
+}
